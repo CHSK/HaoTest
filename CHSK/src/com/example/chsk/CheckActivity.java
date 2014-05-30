ca00e7bf22f48d1c086edafa64995e98a8bd2c9d
danigh <bunnie000@gmail.com>
2014-05-29 22:06:30 -0700

    zipcode implmented

--- CHSK/src/com/example/chsk/CheckActivity.java
+++ CHSK/src/com/example/chsk/CheckActivity.java
@@ -1,5 +1,9 @@
 package com.example.chsk;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.zip.CheckedInputStream;
+
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesClient;
 import com.google.android.gms.location.LocationClient;
@@ -9,9 +13,16 @@ import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -19,6 +30,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.os.Build;
 
@@ -28,23 +41,28 @@ GooglePlayServicesClient.OnConnectionFailedListener {
 	//options is the amount of stuff we can search for
 	//boolean checks is to keep track of the checkboxes that are checked
 	int options = 8;
+	String areaCode = "";
+	boolean zipcode = false;
 	boolean checks[] = new boolean[options];
 	CheckBox checkbox[] = new CheckBox[options];
 	LocationClient mLocationClient;
 	Location currentLocation;
-	
-	
+	Geocoder geocoder = new Geocoder(this);
+	double latitude;
+	double longitude;
+
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_check);
-		
+
 		if (savedInstanceState == null) {
 			getSupportFragmentManager().beginTransaction()
 					.add(R.id.container, new PlaceholderFragment()).commit();
 		}
 		mLocationClient = new LocationClient(this, this, this);
-	
+
 	}
 
 	@SuppressLint("NewApi")
@@ -56,22 +74,113 @@ GooglePlayServicesClient.OnConnectionFailedListener {
 		//returns the btn to its normal color
 		getMenuInflater().inflate(R.menu.login, menu);
 		Button b1 = (Button) findViewById(R.id.quick);
-		Button b2 = (Button) findViewById(R.id.button2);
+		//Button b2 = (Button) findViewById(R.id.button2);
 		 b1.setBackground(getResources().getDrawable(R.drawable.rectangle));
-		 b2.setBackground(getResources().getDrawable(R.drawable.rectangle));
+		 //b2.setBackground(getResources().getDrawable(R.drawable.rectangle));
 		return true;
 	}
-//Go to map
-	
-	
+
+
+
+//this handles entering an area code
+	public void enterArea(final View view)
+	{
+		 
+		
+		AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+
+        LinearLayout layout = new LinearLayout(this);
+        LinearLayout.LayoutParams parms = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        layout.setOrientation(LinearLayout.VERTICAL);
+        layout.setLayoutParams(parms);
+
+        layout.setGravity(Gravity.CLIP_VERTICAL);
+        layout.setPadding(2, 2, 2, 2);
+
+        TextView tv = new TextView(this);
+        tv.setText("Enter an Area Code");
+        tv.setPadding(40, 40, 40, 40);
+        tv.setGravity(Gravity.CENTER);
+        tv.setTextSize(20);
+
+        final EditText et = new EditText(this);
+        
+       
+
+        LinearLayout.LayoutParams tv1Params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        tv1Params.bottomMargin = 5;
+       
+        layout.addView(et, new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+
+        alertDialogBuilder.setView(layout);
+        alertDialogBuilder.setTitle("Area Code");
+        // alertDialogBuilder.setMessage("Input Student ID");
+        alertDialogBuilder.setCustomTitle(tv);
+
+      
+
+        // Setting Negative "Cancel" Button
+        alertDialogBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                dialog.cancel();
+            }
+        });
+
+        // Setting Positive "Yes" Button
+        alertDialogBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+            	try{
+            		areaCode = et.getText().toString();
+            	
+            	}
+            	catch(Exception e)
+            	{
+            		//invaild area code
+            		areaCode = "";
+            	//	 Log.v("bye", "blah blah");
+            	}
+            	if(!areaCode.equals(""))
+            	{
+            		zipcode = true;
+            		goToMap(view);
+            	}
+            }
+        });
+
+        AlertDialog alertDialog = alertDialogBuilder.create();
+
+        try {
+            alertDialog.show();
+        } catch (Exception e) {
+            // WindowManager$BadTokenException will be caught and the app would
+            // not display the 'Force Close' message
+            e.printStackTrace();
+        }
+			}
+		
 	
 	@SuppressLint("NewApi")
 	public void goToMap(View view)
 	{
-		currentLocation = mLocationClient.getLastLocation();
-		double latitude = currentLocation.getLatitude();
-		double longitude = currentLocation.getLongitude();
+		if(zipcode) {
+			try {
+				List<Address> addresses = geocoder.getFromLocationName(areaCode, 1);
+				if(addresses != null && !addresses.isEmpty()) {
+					Address address = addresses.get(0);
+					latitude = address.getLatitude();
+					longitude = address.getLongitude();
+				}
+			} catch (IOException e) {}
+			zipcode = false;
+		}
+		else {
+			currentLocation = mLocationClient.getLastLocation();
+			latitude = currentLocation.getLatitude();
+			longitude = currentLocation.getLongitude();
+		}
 		LatLng cLocation = new LatLng(latitude, longitude);
+		Bundle currentLocation = new Bundle();
+		currentLocation.putParcelable("currentLocation", cLocation);
 
 		TextView tv = new TextView(this);
 		tv=(TextView)findViewById(R.id.textView1);
@@ -83,29 +192,32 @@ GooglePlayServicesClient.OnConnectionFailedListener {
 		checkbox[4] = (CheckBox) findViewById(R.id.CheckDay);
 		checkbox[5] = (CheckBox) findViewById(R.id.CheckNight);
 		checkbox[6] = (CheckBox) findViewById(R.id.CheckFood);
-		checkbox[7] = (CheckBox) findViewById(R.id.CheckFree);
-		
-		
+		checkbox[7] = (CheckBox) findViewById(R.id.CheckFood);
+
+
 		//determines which boxes are checked
 		for(int i = 0; i < options; i ++)
 		{
 			checks[i] = checkbox[i].isChecked();
 		//	tv.append(" " + checks[i]);
 		}
-		
+
 		Button b1 = (Button) findViewById(R.id.quick);
 		b1.setBackground(getResources().getDrawable(R.drawable.rectangleyellow));
-		     
+
 		 Intent intent = new Intent(this, Map.class);
-		         
+		 
+			 intent.putExtra("areaCode", areaCode);
+		 
 		 intent.putExtra("checks", checks);
-		 intent.putExtra("location", cLocation);
+		 
+		 intent.putExtra("location", currentLocation);
 				 //this is the line you need to retrieve the checks in the next intent
 			//	 boolean checks[] = getIntent().getBooleanArrayExtra("checks");
 		 startActivity(intent);
-
+		 Log.v("blah", "blah blah");
 	}
-	
+
 	protected void onStart() {
 		super.onStart();
 		mLocationClient.connect();
--- CHSK/src/com/example/chsk/Map.java
+++ CHSK/src/com/example/chsk/Map.java
@@ -1,16 +1,31 @@
 package com.example.chsk;
 
+import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
-
+import com.google.android.gms.maps.model.MarkerOptions;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -21,20 +36,31 @@ import android.os.Build;
 public class Map extends FragmentActivity {
 
 	private GoogleMap mMap;
+	private JSONArray arrPlace;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_map);
-		setUpMap();
+		try {
+			setUpMap();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	protected void onResume() {
 		super.onResume();
-		setUpMap();
+		try {
+			setUpMap();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
-	private void setUpMap() {
+	private void setUpMap() throws Exception {
 		if(mMap == null){
 			mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
 			if (mMap != null)
@@ -42,13 +68,145 @@ public class Map extends FragmentActivity {
 		}
 	}
 	
-	private void setUpAllMap() {
+	@SuppressLint("NewApi") public JSONObject getJSONFromUrl(String url) {
+		if (android.os.Build.VERSION.SDK_INT > 9) 
+		{
+		    StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+		    StrictMode.setThreadPolicy(policy);
+		}
+	    InputStream is = null;
+		// Making HTTP request
+	    try {
+	      // defaultHttpClient
+	      DefaultHttpClient httpClient = new DefaultHttpClient();
+	      HttpPost httpPost = new HttpPost(url);
+	      HttpResponse httpResponse = httpClient.execute(httpPost);
+	      HttpEntity httpEntity = httpResponse.getEntity();
+	      is = httpEntity.getContent();
+	    } catch (UnsupportedEncodingException e) {
+	      e.printStackTrace();
+	    } catch (ClientProtocolException e) {
+	      e.printStackTrace();
+	    } catch (IOException e) {
+	      e.printStackTrace();
+	    }
+	    String json = null;
+		try {
+	      BufferedReader reader = new BufferedReader(new InputStreamReader(
+	          is, "iso-8859-1"), 8);
+	      StringBuilder sb = new StringBuilder();
+	      String line = null;
+	      while ((line = reader.readLine()) != null) {
+	        sb.append(line);
+	      }
+	      is.close();
+	      json = sb.toString();
+	    } catch (Exception e) {
+	      Log.e("Buffer Error", "Error converting result " + e.toString());
+	    }
+	    JSONObject jObj = null;
+		// try parse the string to a JSON object
+	    try {
+	      jObj = new JSONObject(json);
+	    } catch (JSONException e) {
+	      Log.e("JSON Parser", "Error parsing data " + e.toString());
+	    }
+	    // return JSON String
+	    return jObj;
+	  }
+	
+	private void setUpAllMap() throws Exception {
+		Bundle cLocation = getIntent().getParcelableExtra("location");
+		LatLng currentLocation = cLocation.getParcelable("currentLocation");
 		CameraUpdate center=
-		        CameraUpdateFactory.newLatLng(new LatLng(34.05932,-117.819462));
-		    CameraUpdate zoom=CameraUpdateFactory.zoomTo(15);
-
-		    mMap.moveCamera(center);
-		    mMap.animateCamera(zoom);
+		        CameraUpdateFactory.newLatLng(currentLocation);
+		CameraUpdate zoom=CameraUpdateFactory.zoomTo(10);
+		
+		String keywords = "";
+		boolean checks[] = getIntent().getBooleanArrayExtra("checks");
+		for(int i = 0; i < checks.length; i++){
+			if(checks[i]){
+				switch (i){
+					case 0:
+						keywords = keywords + "aquariam%7Cart_gallery%7Cbar%7Cbowling_alley%7Ccasino%7Cgym%7Clibrary%7Cmuseum%7Cnight_club%7Cshopping_mall%7Cmovie_theatre";
+						break;
+					case 1:
+						keywords = keywords + "amusement_park%7Ccampground%7Czoo%7Cstadium%7Cpark%7C";
+						break;
+					case 2:
+						keywords = keywords + "gym%7Cnight_club%7Camusement_park%7Ccampground%7Czoo%7Cpark%7Cbowling_alley%7C";
+						break;
+					case 3:
+						keywords = keywords + "aquarium%7Cart_gallery%7Cbar%7Ccasino%7Cspa%7Cbeauty_salon%7Clibrary%7Cmovie_theatre%7Cstadium%7Cshopping_mall%7C";
+						break;
+					case 4:
+						keywords = keywords + "park%7Czoo%7Camusement_park%7Cbar%7Ccasino%7Cgym%7Cmovie_theatre%7Cspa%7Cshopping_mall%7Cmuseum%7Ccampground%7C";
+						break;
+					case 5:
+						keywords = keywords + "bar%7Cart_gallery%7Ccampground%7Ccasino%7Cbowling_alley%7Cgym%7Cmovie_theatre%7Cnight_club%7C";
+						break;
+					case 6:
+						keywords = keywords + "restaurant%7C";
+						break;
+					case 7:
+						keywords = keywords + "";
+						break;
+				}
+			}
+		}
+		
+		if(keywords.length() == 0){
+			keywords = "amusemant_park%7Caquariam%7Cart_gallery%7Cbeauty_salon%7Cbowling_alley%7Ccampground%7Ccasino%7Cgym%7Clibrary%7Cmovie_theatre%7Cmuseum%7Cnight_club%7Cpark%7Crestaurant%7Cshopping_mall%7Cspa%7Cstadium%7Czoo%7C";
+		}
+		keywords = keywords.substring(0,keywords.length()-3);
+		System.out.println(keywords);
+		
+		System.out.println("hello");
+//<<<<<<< HEAD
+		JSONObject totalPlace = getJSONFromUrl("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="
+												+ currentLocation.latitude + "," + currentLocation.longitude 
+												+ "&radius=32000&types="
+												+ keywords + "&sensor=false&key=AIzaSyBBGzY_3gkrw6sNUSEcDHRszYjz-Q99PPI");
+		
+		String title, address;
+		LatLng placeLocation;
+		JSONObject temp;
+		//JSONObject totalPlace = new JSONObject(data);
+		arrPlace = totalPlace.getJSONArray("results");
+		for(int i = 0; i < arrPlace.length(); i++){
+			
+			temp = arrPlace.getJSONObject(i);
+			address = temp.getString("vicinity");
+			title = temp.getString("name");
+			temp = temp.getJSONObject("geometry");
+			temp = temp.getJSONObject("location");
+			placeLocation = new LatLng(temp.getDouble("lat"),temp.getDouble("lng"));
+			
+			if(i < arrPlace.length()/3){
+				mMap.addMarker(new MarkerOptions()
+					.position(placeLocation)
+					.title(title)
+					.snippet(address)
+					.icon(BitmapDescriptorFactory.fromResource(R.drawable.greencheck)));
+			}
+			else if(i < arrPlace.length()/3*2){
+				mMap.addMarker(new MarkerOptions()
+					.position(placeLocation)
+					.title(title)
+					.snippet(address)
+					.icon(BitmapDescriptorFactory.fromResource(R.drawable.yellowcheck)));
+			}
+			else{
+				mMap.addMarker(new MarkerOptions()
+					.position(placeLocation)
+					.title(title)
+					.snippet(address)
+					.icon(BitmapDescriptorFactory.fromResource(R.drawable.redcheck)));
+			}
+		}
+		
+		mMap.moveCamera(center);
+		mMap.animateCamera(zoom);
 	}
 	
 	@Override
